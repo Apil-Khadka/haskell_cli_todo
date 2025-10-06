4818b5becd09b75c948b4810b6f253f2fc45bb76
Apil Khadka <apilkhadka2020@gmail.com>
2025-10-06 10:40:09 +0545

    :fire: doing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import System.Environment (getArgs)
+import qualified Data.Text as T
+import Todo
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  args <- getArgs
+
+  saveTodos []
+  
+  case args of
+    ("add":xs)    -> addTodo (T.pack $ unwords xs)
+    ["list"]      -> listTodos
+    ["done", n]   -> markDone (read n)
+    ["delete", n] -> deleteTodo (read n)
+    _             -> putStrLn "Usage: todo [add|list|done|delete] ..."
---
+++ app/Todo.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module Todo where
+
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON, ToJSON, encode, decode)
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
+import System.Directory (doesFileExist)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath (takeDirectory)
+
+data Todo = Todo
+  { todoId    :: Int
+  , task      :: T.Text
+  , completed :: Bool
+  } deriving (Show, Generic)
+
+instance FromJSON Todo
+instance ToJSON Todo
+
+-- File path
+todoFile :: FilePath
+todoFile = "data/todos.json"
+
+-- Load todos from file
+loadTodos :: IO [Todo]
+loadTodos = do
+  exists <- doesFileExist todoFile
+  if exists
+    then do
+      content <- BL.readFile todoFile
+      case decode content of
+        Just todos -> return todos
+        Nothing    -> return []
+    else return []
+
+-- Save todos to file
+saveTodos :: [Todo] -> IO ()
+saveTodos todos = do
+    let dir = takeDirectory todoFile
+    createDirectoryIfMissing True dir   -- create folder if it doesn't exist
+    BL.writeFile todoFile (encode todos)
+
+-- Add a new todo
+addTodo :: T.Text -> IO ()
+addTodo t = do
+  todos <- loadTodos
+  let newId = if null todos then 1 else todoId (last todos) + 1
+      newTodo = Todo newId t False
+  saveTodos (todos ++ [newTodo])
+  putStrLn $ "Added todo: " ++ T.unpack t
+
+-- List all todos
+listTodos :: IO ()
+listTodos = do
+  todos <- loadTodos
+  mapM_ printTodo todos
+  where
+    printTodo (Todo i t c) =
+      putStrLn $ show i ++ ". [" ++ (if c then "x" else " ") ++ "] " ++ T.unpack t
+
+-- Mark todo as done
+markDone :: Int -> IO ()
+markDone tid = do
+  todos <- loadTodos
+  let todos' = map (\t -> if todoId t == tid then t { completed = True } else t) todos
+  saveTodos todos'
+  putStrLn $ "Marked todo " ++ show tid ++ " as done"
+
+-- Delete todo
+deleteTodo :: Int -> IO ()
+deleteTodo tid = do
+  todos <- loadTodos
+  let todos' = filter (\t -> todoId t /= tid) todos
+  saveTodos todos'
+  putStrLn $ "Deleted todo " ++ show tid
